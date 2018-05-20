c2f5089825e2162fbb1ae0cb614b7052a76b9977
z3719629 <412449946@qq.com>
2018-05-20 22:51:43 +0800

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,13 @@
+local module = {}
+module.SSID="七喜"
+module.WIFI_PWD="18626904512"
+
+module.HOST = "183.230.40.39"
+module.PORT = 6002
+module.USERNAME = "产品id" -- chanpin id
+module.PASSWORD = "鉴权信息" --xinxi
+module.CLIENT_ID = "设备id" -- shebei id
+
+module.ENDPOINT = "/nodemcu/"..node.chipid()
+
+module.TOPIC = "topic"
